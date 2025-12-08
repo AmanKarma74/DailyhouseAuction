6032018f7ce0c6bbebd15c5d7d3a719bd32e06e8
Aman <amankarma74@gmail.com>
2025-12-08 13:51:09 +0530

    main

--- src/app/property/[propertyid]/page.jsx
+++ src/app/property/[propertyid]/page.jsx
@@ -29,6 +29,7 @@ async function Property({ params }) {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/api/properties/${propertyid}`,
       { method: "GET", cache: "no-store" }
+      
     );
 
     const data = await res.json();
